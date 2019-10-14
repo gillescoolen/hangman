bf4d4566129df3856ee60fd5e47f46870550b36f
Gilles Coolen <gilleswhc@gmail.com>
2019-10-14 14:05:45 +0200

    Setup game properties

--- src/dev/coolen/Game.java
+++ src/dev/coolen/Game.java
@@ -1,7 +1,44 @@
 package dev.coolen;
 
+/**
+ *  A game consists of a single word. This word is the one the opponent should guess.
+ */
 public class Game {
+    private String word;
+    private Integer guesses;
+
+    public Game(String word) {
+        this.word = word;
+        this.guesses = null;
+    }
+
     /**
-     *  A game consists of a single word. This word is the one the opponent should guess.
+     * @return the word
      */
+    public String getWord() {
+        return word;
+    }
+
+    /**
+     * @param word the word to set
+     */
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    /**
+     * @return the guesses
+     */
+    public Integer getGuesses() {
+        return guesses;
+    }
+
+    /**
+     * @param guesses the guesses to set
+     */
+    public void setGuesses(Integer guesses) {
+        this.guesses = guesses;
+    }
+
+    
 }
