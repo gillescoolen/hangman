a84c4e779c9f5a3e05496a51a43ff4595056aaba
Gilles Coolen <gilleswhc@gmail.com>
2019-10-14 15:24:59 +0200

    Add variable length

--- src/dev/coolen/Hangman.java
+++ src/dev/coolen/Hangman.java
@@ -7,35 +7,71 @@ package dev.coolen;
 public class Hangman {
 
     public void initialize() {
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+        this.printWhiteSpace(50);
         System.out.println("Welkom bij galgje!");
         System.out.println("Bij dit spel moet je het woord van de tegenstander raden.");
         System.out.println("Wanneer je het woord in minder beurten dan je tegenstander raadt, heb je gewonnen!");
+        System.out.println("Allereerst kiezen we de lengte van de woorden. Er mag gekozen worden uit 10 t/m 13.");
 
-        System.out.println("");
+        Integer length = this.setWordLength();
+        System.out.println(String.format("De geselecteerde lengte is %s.", length));
+
+        this.printWhiteSpace(20);
 
         System.out.println("Speler 1, voer je naam in.");
         Player player = this.createPlayer();
         System.out.println(String.format("Welkom, %s!", player.getName()));
 
-        System.out.println("");
+        this.printWhiteSpace(20);
 
         System.out.println("Speler 2, voer je naam in.");
         Player opponent = this.createPlayer();
         System.out.println(String.format("Welkom, %s!", opponent.getName()));
 
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        Match match = new Match(player, opponent);
-        match.start();
+        this.printWhiteSpace(20);
+        Match match = new Match(player, opponent, length);
+        match.create();
     }
 
     private Player createPlayer() {
         String name = System.console().readLine();
 
         return name.equalsIgnoreCase("c") ? new PlayerComputer(name) : new PlayerHuman(name);
+    }
+
+    private Integer setWordLength() {
+        Integer length = null;
+
+        while (length == null) {
+            String potentialLength = System.console().readLine();
+
+            switch (potentialLength) {
+            case "10":
+                length = 10;
+                break;
+            case "11":
+                length = 11;
+
+                break;
+            case "12":
+                length = 12;
+
+                break;
+            case "13":
+                length = 13;
+                break;
+            default:
+                System.out.println("Vul alsjeblieft een nummer tussen 10 t/m 13 in.");
+                break;
+            }
+        }
+
+        return length;
+    }
+
+    private void printWhiteSpace(Integer amount) {
+        for (int i = 0; i < amount; i++) {
+            System.out.println("");
+        }
     }
 }
